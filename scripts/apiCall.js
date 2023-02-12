da01c24d3376da75fffc2bf90fb66b1b6929bc81
MateMarzec <64321779+MateMarzec@users.noreply.github.com>
2023-02-12 22:58:08 +0000

    Add API prompt

--- scripts/apiCall.js
+++ scripts/apiCall.js
@@ -61,11 +61,8 @@ function validation() {
 function promptBuild() {
     let city = document.getElementById("city").value;
     let days = document.getElementById("days").value;
+
     let meals;
-    let budget;
-    let additional = "";
-
-
     if (document.getElementById("meals").value === "yes") {
         let mealsNodeList = document.getElementById("dinnerType").querySelectorAll('input:checked');
 
@@ -75,14 +72,14 @@ function promptBuild() {
 
         const specificMeals = mealsList.toString();
 
-        meals = `please include ${specificMeals} for each day,`
+        meals = `could you consider ${specificMeals} for each day,`
     } else {
         meals = ""
     }
 
-
+    let budget;
     if (document.getElementById("specificBudget").value === "yes") {
-        budget = "please consider the travel budget which is " + document.querySelector('input[name="budget"]:checked').value + ",";
+        budget = "consider travel budget which is " + document.querySelector('input[name="budget"]:checked').value + ",";
     } else {
         budget = "";
     }
@@ -96,17 +93,17 @@ function promptBuild() {
 
         let specificActivities = activitiesList.toString();
 
-        activities = `please include following activities: ${specificActivities}`
+        activities = `if possible include following activities: ${specificActivities}`
     } else {
         activities = ""
     }
 
-
+    let additionally = "";
     if (meals !== "" || budget !== "" || activities !== "") {
-        additional = `Additionally, ${meals} ${budget} ${activities}`
+        additionally = `Additionally, ${meals} ${budget} ${activities}`
     }
 
-    let prompt = `Could you please write a travel guide for ${city} for ${days}. ${additional}`;
+    let prompt = `Could you please write a travel guide in format where each day will be summarized within <p> tag and each location mentioned will be wrapped within <a> tag, with link provided and _blank attribute added. The Travel Guide should be for ${city}, for ${days} days. ${additionally}`;
 
     callApi(prompt);
 }
@@ -122,12 +119,13 @@ function callApi(prompt) {
             },
             body: JSON.stringify({
                 prompt: `${prompt}`,
-                max_tokens: 700,
+                max_tokens: 2000,
             })
         })
         .then(res => res.json())
         .then(data => target.innerText = data.choices[0].text)
-    } catch (error){
+    } catch (err){
         console.log("Fetch Error");
+        prompt("Sorry, we're experiencing some technical difficulties. Our system is unable to go through your request, please try again later");
     }
 }
